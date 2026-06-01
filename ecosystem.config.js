42a5a65315cd54e040f761cf2d39006b9d4bd86e
Your Name <you@example.com>
2026-06-02 03:29:16 +0700

    fix: add cwd to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "shortmax-bot",
       script: "/root/shmdl/venv/bin/python3",
       args: "main.py",
+      cwd: "/root/shmdl",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
