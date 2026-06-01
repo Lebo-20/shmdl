c7066bb109c36cf0c8e82011dd08487e2d18d7a7
Your Name <you@example.com>
2026-06-02 03:24:44 +0700

    fix: use venv python in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "shortmax-bot",
-      script: "python3",
+      script: "/root/shmdl/venv/bin/python3",
       args: "main.py",
       autorestart: true,
       watch: false,
